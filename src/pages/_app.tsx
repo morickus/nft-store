1a5556c615d5a1ff738e3440028d88683581d3b8
mark <enievinas@gmail.com>
2023-02-20 20:46:30 +0600

    Fix font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,6 +22,7 @@ export default function App({Component, pageProps}: AppPropsWithLayout) {
         <title>NFT store</title>
         <meta name="description" content="NFT store"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       {getLayout(<Component {...pageProps} />)}

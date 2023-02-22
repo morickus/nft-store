57106488a873fba76f709253218b71728e144fad
mark <enievinas@gmail.com>
2023-02-22 17:23:47 +0600

    Fix

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,15 +6,16 @@ import classnames from 'classnames'
 interface IButton {
   className?: string
   type?: 'primary' | 'default'
-  size: 'large' | 'middle' | 'small'
+  size?: 'large' | 'middle' | 'small'
 }
 
 const Button: FC<IButton & ButtonProps & PropsWithChildren> = (props) => {
-  const { children, type = 'default', size, className } = props
+  const { children, type = 'default', size = 'middle', className } = props
 
   return (
     <ButtonAntd
-      type="text"
+      size={size}
+      type={type}
       className={classnames(styles.root, {
         [styles[`type_${type}`]]: type,
         [styles[`size_${size}`]]: size,

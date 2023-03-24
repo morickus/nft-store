6455048b724c980c2d2403d2cbd260a3c3626a73
mark <enievinas@gmail.com>
2023-03-24 14:06:50 +0600

    test

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
-const withAntdLess = require('next-plugin-antd-less');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = withAntdLess({
-  // modifyVars: { '@primary-color': '#04f' }, // optional
-  lessVarsFilePath: './src/styles/variables.less', // optional
-  lessVarsFilePathAppendToEndOfContent: false, // optional
-  // optional https://github.com/webpack-contrib/css-loader#object
-  cssLoaderOptions: {
-    // ...
-    mode: "local",
-    localIdentName: "[hash:base64:8]",
-    exportLocalsConvention: "camelCase",
-    exportOnlyLocals: false,
-    // ...
-    getLocalIdent: (context, localIdentName, localName, options) => {
-      return "whatever_random_class_name";
-    },
-  },
-
-  // Other Config Here...
-
-  webpack(config) {
-    return config;
-  },
-});
+module.exports = nextConfig
